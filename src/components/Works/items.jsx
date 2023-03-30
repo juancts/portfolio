6d496935ef75eb169032df117d2e4f729833b292
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-03-30 14:31:28 +0200

    items changes

--- src/components/Works/items.jsx
+++ src/components/Works/items.jsx
@@ -2,8 +2,8 @@
 export const itemData = [
     {
       img: 1,
-      title: 'FullStack development',
-      tech: 'Html, php, MySql, phpMyAdmin',
+      title: 'Logístic & Transport',
+      tech: 'FulStack dev - Html, php, MySql, phpMyAdmin',
       link:'https://ctdominicana.com/'
     },
     {
@@ -16,18 +16,18 @@ export const itemData = [
       img: 3,
       title: 'FullStack Development - Ecommerce',
       tech: 'React, Redux, Nodejs, Postgres, tailwind',
-      link:'https://unbardodesign-front.vercel.app/'
+      link:'https://unbardo.com.ar'
     },
     {
         img: 4,
-        title: 'Maintenance of the web for CTB Group',
-        tech: 'Wordpres',
+        title: 'Logístic & Transport',
+        tech: 'Wordpres - Maintenance of the web',
         link:'https://ctbgroup.es/'
       },
       {
         img: 5,
-        title: 'Full Stack development',
-        tech: 'Html, javascript, php, bootstrap',
+        title: 'Construction & reforms',
+        tech: 'FullStack dev - Html, javascript, php, bootstrap',
         link:'http://artycla.com/'
       }
 
